9abae24320e28d639949a6846dac47f8448c3206
Wirlhawk <rayhantogarlumbantobing@gmail.com>
2025-08-11 11:13:51 +0700

    feat: hero & footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import { Hero7 } from "@/components/hero7";
+import { Footer7 } from "@/components/landing-page/footer7";
+import { Hero7 } from "@/components/landing-page/hero7";
 import Image from "next/image";
 
 export default function Home() {
     return (
-        <div>
-            <Hero7 />
+        <div className="max-w-7xl mx-auto">
+        <Hero7 />
         </div>
     );
 }
